5612c5508956dd114000ddef94aa928d20332d54
faridmau <faridmau@gmail.com>
2025-03-25 08:53:06 +0700

    Add CategoryPolicy and TemplatePolicy for model authorization

---
+++ src/Policies/CategoryPolicy.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Faridmau\AiWritingAssistant\Policies;
+
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+use App\Models\User;
+class CategoryPolicy
+{
+    use HandlesAuthorization;
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user): bool
+    {
+        return true;
+    }
+}
